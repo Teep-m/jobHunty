7c0f2f49537212e9259ae53562ae9aaf50684dc0
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-12-27 23:11:19 +0900

    db setup and random data factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'We are looking for some Laravel developers to be able to develop a web application'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https:www.starkindustries.com',
+        //     'description' => 'Offering a great environment for full-stack Engineers whom are chasing after their dreams!'
         // ]);
     }
 }
